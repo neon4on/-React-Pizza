d659214c94b2b1151b9cc8d60c4cd067ac413d25
Valeriy Kamenskikh <86181396+neon4on@users.noreply.github.com>
2024-02-05 23:48:18 +0400

    Add Adaptive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+
 import Header from './components/Header';
+
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
+
+import { Routes, Route } from 'react-router-dom';
+
 import './scss/app.scss';
 
 function App() {
@@ -10,7 +16,11 @@ function App() {
       <Header />
       <div className="content">
         <div className="container">
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
